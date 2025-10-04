0a3d43e4dcae2f1579ba618467048ef74797fac6
LTQ <loithanhquan@gmail.com>
2025-10-04 14:14:20 +0700

    init commit

---
+++ build/CMakeFiles/hello_window.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_window.dir/src/glad.c.o"
+  "CMakeFiles/hello_window.dir/src/glad.c.o.d"
+  "CMakeFiles/hello_window.dir/src/main.cpp.o"
+  "CMakeFiles/hello_window.dir/src/main.cpp.o.d"
+  "hello_window"
+  "hello_window.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/hello_window.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
