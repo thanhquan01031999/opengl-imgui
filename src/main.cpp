57f08c656139b61a5b64846c35f6300597f2b85c
Loi Quan <loiquan@MacBook-Pro-cua-Loi.local>
2025-10-07 14:38:25 +0700

    3d_sphere change color with imgui

--- src/main.cpp
+++ src/main.cpp
@@ -16,232 +16,267 @@
 #include <glm/gtc/type_ptr.hpp>
 
 // LEARNOPENGL
+#include <learnopengl/camera.h>
 #include <learnopengl/filesystem.h>
 #include <learnopengl/shader.h>
-#include <learnopengl/camera.h>
 
 // STANDARD
-#include <iostream>
-#include <vector>
-
 #include <imgui/imgui.h>
 #include <imgui_impl/glfw.h>
 #include <imgui_impl/opengl3.h>
 
-// CUSTOM
-#include "ltc_matrix.hpp"
-#include "colors.hpp" // LOOK FOR DIFFERENT COLORS!
+#include <iostream>
+#include <vector>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+// CUSTOM
+#include "colors.hpp"  // LOOK FOR DIFFERENT COLORS!
+#include "ltc_matrix.hpp"
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
 
 // settings
 const unsigned int SCR_WIDTH = 600;
 const unsigned int SCR_HEIGHT = 600;
 
-const char *vertexShaderSource ="#version 330 core\n"
+const char *vertexShaderSource =
+    "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
+    "uniform mat4 model;\n"
+    "uniform mat4 view;\n"
+    "uniform mat4 projection;\n"
     "void main()\n"
     "{\n"
-    "   gl_Position = vec4(aPos, 1.0);\n"
+    "gl_Position = projection * view * model * vec4(aPos, 1.0);\n"
     "}\0";
 
-const char *fragmentShaderSource = "#version 330 core\n"
+const char *fragmentShaderSource =
+    "#version 330 core\n"
     "out vec4 FragColor;\n"
     "uniform vec4 ourColor;\n"
     "void main()\n"
     "{\n"
     "   FragColor = ourColor;\n"
     "}\n\0";
-std::vector<float> createCircleVertices(float centerX, float centerY, float radius, int segments) {
-    std::vector<float> vertices;
-    vertices.push_back(centerX); // center x
-    vertices.push_back(centerY); // center y
+// std::vector<float> createCircleVertices(float centerX, float centerY, float
+// radius, int segments) {
+//     std::vector<float> vertices;
+//     vertices.push_back(centerX); // center x
+//     vertices.push_back(centerY); // center y
 
-    for (int i = 0; i <= segments; ++i) {
-        float angle = 2.0f * M_PI * i / segments;
-        float x = centerX + radius * cos(angle);
-        float y = centerY + radius * sin(angle);
-        vertices.push_back(x);
-        vertices.push_back(y);
+//     for (int i = 0; i <= segments; ++i) {
+//         float angle = 2.0f * M_PI * i / segments;
+//         float x = centerX + radius * cos(angle);
+//         float y = centerY + radius * sin(angle);
+//         vertices.push_back(x);
+//         vertices.push_back(y);
+//     }
+//     return vertices;
+// }
+void createSphere(float radius, int sectors, int stacks,
+                  std::vector<float> &vertices,
+                  std::vector<unsigned int> &indices) {
+  float x, y, z, xy;
+  float sectorStep = 2 * M_PI / sectors;
+  float stackStep = M_PI / stacks;
+  float sectorAngle, stackAngle;
+
+  // Generate vertices
+  for (int i = 0; i <= stacks; ++i) {
+    stackAngle = M_PI / 2 - i * stackStep;  // from pi/2 to -pi/2
+    xy = radius * cosf(stackAngle);
+    z = radius * sinf(stackAngle);
+
+    for (int j = 0; j <= sectors; ++j) {
+      sectorAngle = j * sectorStep;
+      x = xy * cosf(sectorAngle);
+      y = xy * sinf(sectorAngle);
+      vertices.push_back(x);
+      vertices.push_back(y);
+      vertices.push_back(z);
     }
-    return vertices;
-}
+  }
 
-int main()
-{
-	// glfw: initialize and configure
-	// ------------------------------
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  // Generate indices
+  for (int i = 0; i < stacks; ++i) {
+    int k1 = i * (sectors + 1);
+    int k2 = k1 + sectors + 1;
+    for (int j = 0; j < sectors; ++j, ++k1, ++k2) {
+      if (i != 0) {
+        indices.push_back(k1);
+        indices.push_back(k2);
+        indices.push_back(k1 + 1);
+      }
+      if (i != (stacks - 1)) {
+        indices.push_back(k1 + 1);
+        indices.push_back(k2);
+        indices.push_back(k2 + 1);
+      }
+    }
+  }
+}
+int main() {
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 #ifdef __APPLE__
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-	// glfw window creation
-	// --------------------
-	GLFWwindow *window = glfwCreateWindow(
-		SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL: Area Lights", NULL, NULL);
-	if (window == NULL)
-	{
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+  GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT,
+                                        "LearnOpenGL: Area Lights", NULL, NULL);
+  if (window == NULL) {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	// tell GLFW to capture our mouse
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO &io = ImGui::GetIO();
 
-	// glad: load all OpenGL function pointers
-	// ---------------------------------------
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
-		std::cout << "Failed to initialize GLAD" << std::endl;
-		return -1;
-	}
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGuiIO &io = ImGui::GetIO();
+  ImGui::StyleColorsDark();
 
-	ImGui::StyleColorsDark();
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  ImGui_ImplOpenGL3_Init("#version 330");
 
-	ImGui_ImplGlfw_InitForOpenGL(window, true);
-	ImGui_ImplOpenGL3_Init("#version 330");
+  unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+  int success;
+  char infoLog[512];
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+              << infoLog << std::endl;
+  }
+  // fragment shader
+  unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  glCompileShader(fragmentShader);
+  // check for shader compile errors
+  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
+              << infoLog << std::endl;
+  }
+  // link shaders
+  unsigned int shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+  // check for linking errors
+  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+  if (!success) {
+    glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n"
+              << infoLog << std::endl;
+  }
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
 
-	// configure global opengl state
-	// -----------------------------
-    // build and compile our shader program
-    // ------------------------------------
-    // vertex shader
-    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-    // check for shader compile errors
-    int success;
-    char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-    // fragment shader
-    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-    // check for shader compile errors
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-    // link shaders
-    unsigned int shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    // check for linking errors
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-    }
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
+  std::vector<float> vertices;
+  std::vector<unsigned int> indices;
+  createSphere(1.0f, 4, 4, vertices, indices);
 
-    // set up vertex data (and buffer(s)) and configure vertex attributes
-    // ------------------------------------------------------------------
-    auto vertices = createCircleVertices(0.0f, 0.0f, 0.5f, 100);
+  unsigned int VBO, VAO, EBO;
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
 
-    unsigned int VBO, VAO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+  glBindVertexArray(VAO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float),
+               vertices.data(), GL_STATIC_DRAW);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int),
+               indices.data(), GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+
+  glEnable(GL_DEPTH_TEST);
+
+  static float redValue = 0.0f;
+  static float greenValue = 0.0f;
+  static float blueValue = 0.0f;
+  while (!glfwWindowShouldClose(window)) {
+    // Start IMGUI Frame
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    // input
+    // -----
+    processInput(window);
+    ImGui::Begin("Hello Quan");
+    ImGui::Text("Helloooo");
+    // ImGui::InputFloat("Color",&l_color,1.0f,1.0f,1,false);
+    ImGui::DragFloat("red", &redValue, 0.01f, 0.0f, 1.0f);
+    ImGui::DragFloat("green", &greenValue, 0.01f, 0.0f, 1.0f);
+    ImGui::DragFloat("blue", &blueValue, 0.01f, 0.0f, 1.0f);
+    ImGui::End();
+    ImGui::Render();
+
+    // render
+    // ------
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glUseProgram(shaderProgram);
+    int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+    glUniform4f(vertexColorLocation, redValue, greenValue, blueValue, 1.0f);
+
+    // Transformation
+    glm::mat4 model = glm::rotate(glm::mat4(1.0f), (float)glfwGetTime(),
+                                  glm::vec3(0.0f, 1.0f, 0.0f));
+    glm::mat4 view =
+        glm::lookAt(glm::vec3(0.0f, 0.0f, 3.0f), glm::vec3(0.0f, 0.0f, 0.0f),
+                    glm::vec3(0.0f, 1.0f, 0.0f));
+    glm::mat4 projection =
+        glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+
+    GLuint modelLoc = glGetUniformLocation(shaderProgram, "model");
+    GLuint viewLoc = glGetUniformLocation(shaderProgram, "view");
+    GLuint projLoc = glGetUniformLocation(shaderProgram, "projection");
+
+    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+    glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+    glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
     glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-    // glBindVertexArray(0);
-
-
-    // bind the VAO (it was already bound, but just to demonstrate): seeing as we only have a single VAO we can
-    // just bind it beforehand before rendering the respective triangle; this is another approach.
-    glBindVertexArray(VAO);
-	// RENDER LOOP
-	static float redValue = 0.0f;
-	static float greenValue = 0.0f;
-	static float blueValue = 0.0f;
-	while (!glfwWindowShouldClose(window))
-	{
-		// Start IMGUI Frame
-		ImGui_ImplOpenGL3_NewFrame();
-		ImGui_ImplGlfw_NewFrame();
-		ImGui::NewFrame();
-
-		        // input
-        // -----
-        processInput(window);
-		ImGui::Begin("Hello Quan");
-		ImGui::Text("Helloooo");
-		// ImGui::InputFloat("Color",&l_color,1.0f,1.0f,1,false);
-		ImGui::DragFloat("red", &redValue, 0.01f, 0.0f, 1.0f);
-		ImGui::DragFloat("green", &greenValue, 0.01f, 0.0f, 1.0f);
-		ImGui::DragFloat("blue", &blueValue, 0.01f, 0.0f, 1.0f);
-		ImGui::End();
-		ImGui::Render();
-
-        // render
-        // ------
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glUseProgram(shaderProgram);
-
-
-        // update shader uniform
-        // double  timeValue = glfwGetTime();
-        // float greenValue = static_cast<float>(sin(timeValue) / 2.0 + 0.5);
-        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-        glUniform4f(vertexColorLocation, redValue, greenValue, blueValue, 1.0f);
-
-        // render the triangle
-        glDrawArrays(GL_TRIANGLE_FAN, 0, vertices.size()/2);
-		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-
-        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-        // -------------------------------------------------------------------------------
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-	}
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-    glDeleteProgram(shaderProgram);
-	glfwTerminate();
-	return 0;
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
+  glDeleteProgram(shaderProgram);
+  glfwTerminate();
+  return 0;
 }
-void processInput(GLFWwindow *window)
-{
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
+void processInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
 }
 
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// glfw: whenever the window size changed (by OS or user resize) this callback
+// function executes
 // ---------------------------------------------------------------------------------------------
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    // make sure the viewport matches the new window dimensions; note that width and
-    // height will be significantly larger than specified on retina displays.
-    glViewport(0, 0, width, height);
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  // make sure the viewport matches the new window dimensions; note that width
+  // and height will be significantly larger than specified on retina displays.
+  glViewport(0, 0, width, height);
 }
